60d16121165a8afa9041aa8402949c47ea641e1f
Nehaingale96 <136497802+Nehaingale96@users.noreply.github.com>
2023-08-05 17:02:07 +0530

    Your commit message here

--- src/project2/Form.js
+++ src/project2/Form.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import { Form,Container, Row, Col } from 'react-bootstrap';
 import './Form.css';
 
-function Form() {
+function FormComponent() {
   const [selectedOption, setSelectedOption] = useState('');
   const [isFormComplete, setIsFormComplete] = useState(false);
   const [firstName, setFirstName] = useState('');
@@ -42,80 +43,85 @@ function Form() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className='container'>
-      <div className='wrapper'>
-        <label className='firstName'>
-          First Name<br />
-          <input
-            type='text'
-            value={firstName}
-            className='click'
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </label>
-        <label className='lastName'>
-          Last Name<br />
-          <input
-            type='text'
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            className='click'
-          />
-        </label>
-      </div>
-      <div className='radioWrapper'>
-        <h1>How should we contact you?</h1>
-        <div>
-          <label style={{margin:"20px"}} className='label'>
-            <input
+    <Container className='container' style={{marginTop:'50px',boxShadow:'5px 5px 10px gray'}} >
+      <Form onSubmit={handleSubmit}>
+        <Row className='wrapper' style={{marginTop:'30px'}}>
+          <Col xs={12} md={6}>
+            <Form.Group controlId='firstName'>
+              <Form.Label className='d-inline'>First Name</Form.Label>
+              <Form.Control
+                type='text'
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className='click d-inline'
+              />
+            </Form.Group>
+          </Col>
+          <Col xs={12} md={6}>
+            <Form.Group controlId='lastName'>
+              <Form.Label className='d-inline'>Last Name</Form.Label>
+              <Form.Control
+                type='text'
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                className='click d-inline'
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row className='wrapper'>
+          <Col xs={12}  md={6} >
+          <h4 style={{marginTop:'40px'}}>How should we contact you?</h4>
+          <Form.Group style={{marginBottom:'50px'}}>
+            <Form.Check
               type='radio'
+              label='Phone Number'
               value='number'
               checked={selectedOption === 'number'}
               onChange={handleOptionChange}
-              className='radio'
+              className='radio d-inline'
             />
-            Phone Number
-          </label>
-        </div>
-        <div>
-          <label className='label'>
-            <input
+            <Form.Check
               type='radio'
+              label='Email Address'
               value='email'
               checked={selectedOption === 'email'}
               onChange={handleOptionChange}
-              className='radio'
+              className='radio d-inline'
             />
-            Email Address
-          </label>
-        </div>
-      </div>
-      {selectedOption === 'number' && (
-        <div className='item'>
-          <label >Phone Number</label>
-          <input
-            type='number'
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-        </div>
-      )}
-      {selectedOption === 'email' && (
-        <div className='item'>
-          <label style={{ margin: '20px' }}>Email Address</label>
-          <input
-            type='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-      )}
-      {/* {isFormComplete === false && (
-        <p style={{ color: 'red' }}>Please fill all details.</p>
-      )} */}
-      <button type='submit' style={{margin:'80px'}}>Submit</button>
-    </form>
+          </Form.Group>
+        </Col>
+        <Col xs={12} md={6} style={{marginTop:'20px'}}>
+        {selectedOption === 'number' && (
+          <Form.Group className='item'>
+            <Form.Label className='d-inline'>Phone Number</Form.Label>
+            <Form.Control
+              type='number'
+              value={phoneNumber}
+              className='click d-inline'
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+          </Form.Group>
+        )}
+        
+        {selectedOption === 'email' && (
+          <Form.Group className='item'>
+            <Form.Label className='d-inline'>Email Address</Form.Label>
+            <Form.Control
+              type='email'
+              value={email}
+              className='click d-inline'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+        )}
+        </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 }
 
-export default Form;
+export default FormComponent;
+
+
